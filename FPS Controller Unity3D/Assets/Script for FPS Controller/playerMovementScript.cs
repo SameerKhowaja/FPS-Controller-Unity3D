19872cc59f4cbfb5485bc8913b42fdab92810b05
Sameer Khowaja <sameerkhowaja12@gmail.com>
2020-03-20 21:42:26 +0500

    Complete work 1.0

---
+++ FPS Controller Unity3D/Assets/Script for FPS Controller/playerMovementScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovementScript : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform groundCheck;
+    public LayerMask groundMask;
+
+    public float speed;
+    public float gravity;
+    public float groundDistance;
+    public float jumpHeight;
+
+    bool isGrounded;
+
+    Vector3 velocity;
+
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -1 * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
